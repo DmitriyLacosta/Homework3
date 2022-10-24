e87b330226f0697e43668c4bfdb9dd7b265203a9
Дмитрий <dkovaloff@gmail.com>
2022-10-25 01:45:16 +0500

    задача19hard

---
+++ Задача19Hard/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 19 - HARD необязательная
+// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет, является ли оно палиндромом. Через строку нельзя решать само собой.
+
+Console.WriteLine("Программа, принимает на вход целое число любой разрядности и проверяет, является ли оно палиндромом.");
+
+Console.Write("Введите число: ");
+int input = int.Parse(Console.ReadLine()!);
+int temp = input;
+
+Console.Write("Введите количество цифр в числе: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int[] firstArray = new int[N];
+for (int i = 0; i < N; i++)
+{
+    firstArray[i] = input % 10;
+    input = input / 10;
+}
+
+int[] secondArray = new int[N];
+for (int i = N - 1; i >= 0; i--)
+{
+    secondArray[i] = temp % 10;
+    temp /= 10;
+}
+
+bool isEqual = Enumerable.SequenceEqual(firstArray, secondArray);
+
+if (isEqual)
+{
+    Console.WriteLine("Число является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом.");
+}
